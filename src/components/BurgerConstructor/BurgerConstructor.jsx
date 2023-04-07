02d0cf61dc314e453580e374d86b5e6a27567e48
Miroshnikov M <dev.miroshnikov.m@gmail.com>
2023-04-08 01:57:30 +0300

    add button to burgeringredients

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -1,30 +1,43 @@
 import styles from "./styles.module.css";
 import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
 import { DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
 
 function BurgerConstructor(props) {
   const { elements } = props;
 
   return (
-    <ul className={styles.box}>
-      {elements.map((element) => {
-        return (
-          <li
-            className={`${styles.dragIconConstructorElementWrapper} mb-4`}
-            key={element._id}
-          >
-            <DragIcon />
+    <div className={styles.box + " pt-25"}>
+      <ul className={styles.list}>
+        {elements.map((element) => {
+          return (
+            <li
+              className={`${styles.dragIconConstructorElementWrapper} mb-4`}
+              key={element._id}
+            >
+              <DragIcon />
 
-            <ConstructorElement
-              isLocked={true}
-              text={element.name}
-              price={element.price}
-              thumbnail={element.image}
-            />
-          </li>
-        );
-      })}
-    </ul>
+              <ConstructorElement
+                isLocked={true}
+                text={element.name}
+                price={element.price}
+                thumbnail={element.image}
+              />
+            </li>
+          );
+        })}
+      </ul>
+      <div className={styles.bottomElementsWrapper + " mt-10"}>
+        <p className="text text_type_digits-medium">610</p>
+        <span className="ml-2 mr-10">
+          <CurrencyIcon type="primary" />
+        </span>
+        <Button htmlType="button" type="primary" size="medium">
+          Оформить заказ
+        </Button>
+      </div>
+    </div>
   );
 }
 
